4e1057ad390817a9a468c6a0f1e35df164b5278f
ZahwaAulia <zahwa24ti@mahasiswa.pcr.ac.id>
2025-10-15 09:31:04 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PegawaiController;
-use App\Http\Controllers\DashboardController;
+
 Route::get('/', function () {
     return 'Selamat Datang di Website Kampus PCR!';
 
@@ -42,7 +42,7 @@ Route::get('/matakuliahdestroy', [MatakuliahController::class, 'destroy']);
 
 Route::get('/matakuliah/show/{kode?}', [MatakuliahController::class, 'show']);
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index']);
 
 Route::post('question/store', [QuestionController::class, 'store'])
     ->name('question.store');
@@ -57,8 +57,7 @@ Route::get('/auth', function () {
 });
 Route::get('/auth', [AuthController::class, 'index']);
 Route::post('/auth/login', [AuthController::class, 'login']);
-Route::get('/dashboard11', function () {
-    return view('dashboard111');
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
 Route::get('/pegawai', [PegawaiController::class, 'index']);
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
