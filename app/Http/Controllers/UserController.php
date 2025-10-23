a8102bc465859d2590c0b0dfcf9b8f2429760dce
zahwaaulia <zahwa24ti@mahasiswa.pcr.ac.id>
2025-10-23 10:45:02 +0700

    p7 baruuuuu

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,84 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data['dataUser'] = User::all();
+        return view('admin.user.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data             = $request->all();
+        $data['password'] = Hash::make($request->password);
+
+        User::create($data);
+
+        return redirect()->route('user.index')->with('success', 'User berhasil ditambahkan!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $data['dataUser'] = User::findOrFail($id);
+        return view('admin.user.edit', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $request->all();
+
+        // Password hanya diupdate jika form password tidak kosong
+        if ($request->password) {
+            $data['password'] = Hash::make($request->password);
+        } else {
+            unset($data['password']);
+        }
+
+        User::findOrFail($id)->update($data);
+
+        return redirect()->route('user.index')->with('success', 'User berhasil diperbarui!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return redirect()->route('user.index')->with('success', 'User berhasil dihapus!');
+    }
+}
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -69,6 +69,18 @@
                      <span class="sidebar-text">Pelanggan</span>
                  </a>
              </li>
+             <li class="nav-item {{ request()->routeIs('user.*') ? 'active' : '' }}">
+                 <a href="{{ route('user.index') }}" class="nav-link">
+                     <span class="sidebar-icon">
+                         <svg class="icon icon-xs me-2" fill="none" stroke="currentColor" stroke-width="1.5"
+                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                             <path stroke-linecap="round" stroke-linejoin="round"
+                                 d="M15.75 6.75a3 3 0 11-6 0 3 3 0 016 0zM4.5 20.25a7.5 7.5 0 1115 0v.75H4.5v-.75z" />
+                         </svg>
+                     </span>
+                     <span class="sidebar-text">User</span>
+                 </a>
+             </li>
 
              <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
              <li class="nav-item">
